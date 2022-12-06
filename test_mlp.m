3647c206369d5a6682f2dd3bcfca2dae9280d22d
JoelPulliainen <joelpulliainen@hotmail.com>
2022-12-06 23:14:33 +0200

    Fixed typo

--- test_mlp.m
+++ test_mlp.m
@@ -2,14 +2,13 @@ clc
 clear all
 close 
 
-
 %% Data collection and training set new values
 
 [data,class] = data_collect();
 [traindata,trainclass, validationdata,validationclass] = data_splitter(data,class);
 
 %% Test with the validation set
-[Predicted_digits,Accuracy] = mlp_tester(validationdata,validationclass)
+[Predicted_digits,Accuracy] = mlp_test(validationdata,validationclass)
 
 %% Test with  all
 [Predicted_digits_all,Accuracy_all] = mlp_test(data,class)
