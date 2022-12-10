4e9d5565c834dc0cd54ca577da9327c294873ddd
JoelPulliainen <joelpulliainen@hotmail.com>
2022-12-10 18:19:36 +0200

    add files

---
+++ digit_classify_2.m
@@ -0,0 +1,42 @@
+function C = digit_classify_2(testdata,weightHidden1,weightHidden2,weightOutput)
+
+    % Classify digits written with LeapMotion sensor
+    % Input N x 3 points
+    % Output C digit
+    % Adds data to the original to make it 1000x3 matrix
+    % Calculate distinct features of digits
+    % Create 30x30 matrix that represents the digit
+    % Flatten the 30x30 matrix and feed it to a MLP with 2 hidden layers
+    % Output a prediction of the digit
+
+    % Get the weights of the neurons
+
+    % Enhance, add and extract the features
+    testdata = feature_enhancer(testdata);
+    testdata_stats = feature_adder(testdata);
+    testdata = feature_extractor(testdata);
+    testdata = [testdata_stats;testdata];
+
+    % Add bias
+    bias = 0;
+    testdata = [testdata; bias];
+
+    % Classify
+    % Feed forward values to first hidden layer, use activation function relu and add bias
+    Hidden1 = weightHidden1'*testdata;
+    Hidden1 = reLu(Hidden1);
+    Hidden1 = [Hidden1; bias];
+
+    % Feed forward values to second hidden layer, use activation function relu and add bias
+    Hidden2 = weightHidden2'*Hidden1;
+    Hidden2 = reLu(Hidden2);
+    Hidden2 = [Hidden2; bias];
+
+    % Feed forward values to output layer, use activation softmax
+    Output = weightOutput'*Hidden2;
+    Output = softmax(Output);
+
+    [confidence, predicted_class] = max(Output, [], 1);
+    C = predicted_class-1; % -1 because indexing
+
+end
---
+++ mlp_test_2.m
@@ -0,0 +1,21 @@
+function [pred_matrix,Accuracy_percentage] = mlp_test_2(testdata,testclass,weightHidden1,weightHidden2,weightOutput)
+    [m,n] = size(testdata);
+    
+    % Test the validation set
+    for i = 1:n
+        for j = 1:m
+            n_testdata = cell2mat(testdata(j,i));
+            n_testdata = n_testdata.pos;
+            predclass = digit_classify_2(n_testdata,weightHidden1,weightHidden2,weightOutput);
+            realclass(j,i) = testclass(j,i);
+            pred_matrix(j,i) = max(predclass);
+        end
+    end
+
+    % Calculate accuracy
+
+    accuracy1 = realclass == pred_matrix;
+    correct_classifications = sum(accuracy1,"all");
+    incorrect = size(realclass,1)*size(realclass,2);
+    Accuracy_percentage = correct_classifications/incorrect
+end
